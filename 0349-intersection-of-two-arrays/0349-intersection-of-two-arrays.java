d76a973c9668352d000fb403a99bc927ad166248
Ananya Awasthi <ananyaawasthi35@gmail.com>
2026-09-17 21:27:33 +0530

    Time: 3 ms (47.25%), Space: 44.6 MB (88.1%) - LeetHub

---
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        int k=0;
+        int arr[]=new int[nums1.length];
+        for(int i=0;i<nums1.length;i++){
+            boolean found=false;
+            for(int j=0;j<nums2.length;j++){
+                if(nums1[i]==nums2[j]){
+                    found=true;
+                    break;
+                }
+            }
+           
+            if(found){
+                 boolean duplicate=false;
+                for(int x=0;x<k;x++){
+                    if(arr[x]==nums1[i]){
+                        duplicate=true;
+                        break;
+                    }
+                }
+                if(!duplicate){
+                    arr[k]=nums1[i];
+                        k++;
+                    
+                }
+            }
+        }
+        return Arrays.copyOf(arr,k);
+        
+        
+    }
+}

18e7f5c8b3737a8f9f241f61d3466abc92591145
Ananya Awasthi <ananyaawasthi35@gmail.com>
2026-09-22 00:38:43 +0530

    Time: 7 ms (16.77%), Space: 47.7 MB (12.36%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -2,7 +2,7 @@ class Solution {
     public int missingNumber(int[] nums) {
         HashMap<Integer, Integer> map=new HashMap<>();
         for(int x:nums){
-        map.put(x,map.getOrDefault(x,0)+1);}
+        map.put(x,map.getOrDefault(0,x)+1);}
         for(int x=0;x<=nums.length;x++){
             if(!map.containsKey(x)){
                 return x;
